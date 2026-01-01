822760cb07642cdb1f6ecfb16f81840dcd62a11a
sakshi kachate <sakshikachate347@gmail.com>
2026-01-01 07:16:10 -0800

    each ele multiply by 2

---
+++ Assignment/Assignment48/program1_3.c
@@ -0,0 +1,178 @@
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+//      Required header file :
+//
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+//      Declaration of Structure:
+//      it contain : 
+//                      data of node 
+//                      next pointer to points another node
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+struct node 
+{
+    int data;
+    struct node * next;
+
+};
+
+    typedef struct node NODE;
+    typedef struct node * PNODE;
+    typedef struct node ** PPNODE;
+
+
+     void Display(PNODE first)
+    {
+        while(first != NULL)
+        {
+            printf("|%d|->",first ->data);
+            first = first->next;
+        }
+        printf("NULL \n");
+    }
+
+int count(PNODE first)
+{
+    int icount = 0;
+    while(first != NULL)
+    {
+        icount++;
+        first = first->next;
+    }
+    return icount;
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = *first;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if (*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : MultiplyByTwo
+//      Desciption    : use to display multiplay value of all ele   with 2
+//      Input         : ptr**
+//      output        :  int
+//      Author name   : Sakshi kachate
+//      Date          : 1/1/26 
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+
+void MultiplyByTwo(PPNODE first )
+{
+     
+    PNODE temp = NULL;
+    temp = *first;
+    
+     while ( temp != NULL)
+    {
+        
+         
+        {
+            temp->data = temp->data * 2;
+        }
+            temp = temp->next;
+
+            
+    }
+    
+         
+}
+
+
+
+
+      
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+//
+//      Entry point Function
+//
+//
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+int main()
+{
+
+    PNODE head = NULL;
+     
+    InsertLast(&head ,2);
+    InsertLast(&head ,3);
+    InsertLast(&head ,4);
+    InsertLast(&head ,5);
+
+
+     
+
+    printf("original :\n");
+    Display(head ); 
+
+    MultiplyByTwo(&head);
+
+    printf("each ele multiply with 2:\n");
+    Display(head );
+
+    
+    
+
+ 
+    return 0;
+}
+
+
+
+
+
+
+
+/*
+
+original :
+|2|->|3|->|4|->|5|->NULL
+each ele multiply with 2:
+|4|->|6|->|8|->|10|->NULL
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+ 
+
+*/
