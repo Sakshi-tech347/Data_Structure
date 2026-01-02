76cebb5056ed2780c7795ee7c0e2a819236a5103
sakshi kachate <sakshikachate347@gmail.com>
2026-01-02 04:29:52 -0800

    Display Alternate mode

---
+++ Assignment/Assignment50/program1_4.c
@@ -0,0 +1,185 @@
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+//      Required header file :
+//
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+//      Declaration of Structure:
+//      it contain : 
+//                      data of node 
+//                      next pointer to points another node
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+struct node 
+{
+    int data;
+    struct node * next;
+
+};
+
+    typedef struct node NODE;
+    typedef struct node * PNODE;
+    typedef struct node ** PPNODE;
+
+   void Display(PNODE first)
+    {
+        while(first != NULL)
+        {
+            printf("|%d|->",first ->data);
+            first = first->next;
+        }
+        printf("NULL \n");
+    }
+
+
+int count(PNODE first)
+{
+    int icount = 0;
+    while(first != NULL)
+    {
+        icount++;
+        first = first->next;
+    }
+    return icount;
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = *first;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if (*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name :DisplayAlternat
+//      Desciption    : use to  Display Alternate node
+//                                 
+//      Input         : 
+//      output        :  
+//      Author name   : Sakshi kachate
+//      Date          : 2/1/26
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//
+
+    void DisplayAlternat(PNODE first)
+    {
+         
+         PNODE temp = NULL;
+        temp = first;
+        
+        
+        if (first == NULL) 
+        {
+            return ; 
+        }
+       
+         
+
+       
+        while(first != NULL)
+        {
+            printf("|%d|->",first ->data);
+            first = first->next->next;
+        }
+        printf("NULL \n");
+
+        
+        
+    }
+    
+ 
+
+
+
+
+      
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+//
+//      Entry point Function
+//
+//
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+int main()
+{
+
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertLast(&head ,5);
+    InsertLast(&head ,3);
+    InsertLast(&head ,10);
+    InsertLast(&head ,6);
+    InsertLast(&head ,15);
+    InsertLast(&head ,4);
+
+
+
+
+     
+
+    printf("original :\n");
+    Display(head ); 
+
+     
+
+    printf("after :\n");
+  
+   DisplayAlternat(head );
+
+   
+
+    
+ 
+    return 0;
+}
+
+
+/*
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+                   
+
+original :
+|5|->|3|->|10|->|6|->|15|->|4|->NULL
+after :
+|5|->|10|->|15|->NULL                            // alternate node 
+*/
