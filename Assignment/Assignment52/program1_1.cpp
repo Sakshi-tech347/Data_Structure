2a05e92a5e695874a6b14cce3e989c3eac0bb03d
sakshi kachate <sakshikachate347@gmail.com>
2026-01-10 10:04:13 -0800

    Implement template Multiply function
    Added a template function to multiply two numbers of any type and updated the main function to demonstrate its usage with both integers and floats.

--- Assignment/Assignment52/program1_1.cpp
+++ Assignment/Assignment52/program1_1.cpp
@@ -1,2 +1,40 @@
 #include<iostream>
 using namespace std;
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//      function name : Multiply
+//      Description   : multiply two numbers
+//      Input         : T , T
+//      Output        : T
+//      Auther name   : Sakshi Laxman Kachate.
+//      Date          : 10/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////
+template<class T>
+T Multiply(T No1 , T No2)
+{
+    T Ans;
+    Ans = No1 * No2;
+    return Ans;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+//      ENTRY POINT FUNCTION
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+    int iRet = 0;
+    iRet = Multiply(10,20);
+
+    cout<<iRet<<"\n";
+    
+    float fRet =Multiply(10.0f,20.0f);
+    cout<<iRet<<"\n";
+    return 0;
+}
